8067636e3bf12eebd2c644aa96a609f5806a3aed
Ubuntu <ktmrmshk@gmail.com>
2023-09-09 12:00:02 +0900

    added output.tf

---
+++ modules/aws_vpc/output.tf
@@ -0,0 +1,3 @@
+output "vpc_id" { value = module.vpc.vpc_id }
+output "default_route_table_id" { value = module.vpc.default_route_table_id }
+output "default_security_group_id" { value = module.vpc.default_security_group_id }

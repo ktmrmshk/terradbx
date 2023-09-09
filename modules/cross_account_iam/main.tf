d4cae89501be692a5be248904a35e36b44eb8bb4
Ubuntu <ktmrmshk@gmail.com>
2023-09-09 16:13:13 +0900

    module: cross_account_iam added

---
+++ modules/cross_account_iam/main.tf
@@ -0,0 +1,37 @@
+data "databricks_aws_assume_role_policy" "this" {
+  external_id = var.databricks_account_id
+}
+
+resource "aws_iam_role" "cross_account_role" {
+  name               = "${var.prefix}-crossaccount"
+  assume_role_policy = data.databricks_aws_assume_role_policy.this.json
+}
+
+data "databricks_aws_crossaccount_policy" "this" {
+}
+
+resource "aws_iam_role_policy" "this" {
+  name   = "${var.prefix}-policy"
+  role   = aws_iam_role.cross_account_role.id
+  policy = data.databricks_aws_crossaccount_policy.this.json
+}
+
+
+//Delay for workaround - cross account iam validation by databricks
+resource "time_sleep" "wait_10sec" {
+  depends_on = [aws_iam_role_policy.this]
+
+  create_duration = "10s"
+}
+
+resource "databricks_mws_credentials" "this" {
+  provider         = databricks.mws
+  account_id       = var.databricks_account_id
+  role_arn         = aws_iam_role.cross_account_role.arn
+  credentials_name = "${var.prefix}-creds"
+  depends_on       = [aws_iam_role_policy.this, time_sleep.wait_10sec]
+}
+
+
+
+
---
+++ modules/cross_account_iam/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {}
+variable "databricks_account_username" {}
+variable "databricks_account_password" {}
+variable "databricks_account_id" {}
+variable "prefix" {}

c8426d6d1c78e76d8599f4fe0d6701d93b511f8f
Ubuntu <ktmrmshk@gmail.com>
2023-09-10 08:21:53 +0900

    mod

--- env/example_workspaces/shared_resources/main.tf
+++ env/example_workspaces/shared_resources/main.tf
@@ -19,10 +19,3 @@ module "example_cred" {
 }
 
 
-output "vpc_id" { value = module.exapmle_vpc.vpc_id }
-output "default_route_table_id" { value = module.exapmle_vpc.default_route_table_id }
-output "default_security_group_id" { value = module.exapmle_vpc.default_security_group_id }
-output "route_table_ids" { value = module.exapmle_vpc.route_table_ids }
-output "selected_azs" { value = module.exapmle_vpc.selected_azs }
-
-output "credentials_id" { value = module.example_cred.credentials_id }

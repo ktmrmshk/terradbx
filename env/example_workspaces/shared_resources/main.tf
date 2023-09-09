02482300262f50761d257d03680fd9e735301fc6
Ubuntu <ktmrmshk@gmail.com>
2023-09-09 23:22:12 +0900

    mod

--- env/example_workspaces/shared_resources/main.tf
+++ env/example_workspaces/shared_resources/main.tf
@@ -1,26 +1,3 @@
-variable "prefix" { default = "ktmrdbx" }
-variable "aws_region" { default = "ap-northeast-1" }
-variable "cidr" { default = "10.99.0.0/16" }
-variable "private_subnets_for_endpoints" { default = ["10.99.254.0/24", "10.99.253.0/24"] }
-variable "public_subnets_for_natgw_per_az" { default = ["10.99.252.0/24", "10.99.251.0/24"] }
-variable "databricks_account_username" {}
-variable "databricks_account_password" {}
-variable "databricks_account_id" {}
-
-
-resource "random_string" "randfix" {
-  special = false
-  upper   = false
-  length  = 8
-}
-
-locals {
-
-  prefix = "${var.prefix}-${random_string.randfix.result}"
-}
-
-
-
 module "exapmle_vpc" {
   source = "../../modules/aws_vpc"
 
